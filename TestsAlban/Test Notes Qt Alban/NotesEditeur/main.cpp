12b3869db4e3c486998ea014691b38bbb939333a
XxOrrorinxX <alban.toumani@gmail.com>
2017-06-12 18:29:17 +0200

    ok

--- TestsAlban/Test Notes Qt Alban/NotesEditeur/main.cpp
+++ TestsAlban/Test Notes Qt Alban/NotesEditeur/main.cpp
@@ -12,17 +12,19 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QString fichier = QFileDialog::getOpenFileName();
+    QString fichier = QFileDialog::getOpenFileName(nullptr,"Selectionner le fichier de Notes");
     NotesManager& m=NotesManager::getInstance();
     m.load(fichier);
     m.setFilename(fichier);
 
-    QString fichier2 = QFileDialog::getOpenFileName();
+    QString fichier2 = QFileDialog::getOpenFileName(nullptr,"Selectionner le fichier de Couples");
     CoupleManager& m2=CoupleManager::getInstance();
     m2.load(fichier2);
     //m2.setFilename(fichier2);
     
     FenPrincipale& fenetre=FenPrincipale::getInstance();
+   // fenetre.setMinimumSize(1000,650);
+    fenetre.setWindowState(Qt::WindowMaximized);
     fenetre.show();
 
     return app.exec();
